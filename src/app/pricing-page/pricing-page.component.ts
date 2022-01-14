a3f2b094cbc23b5df465a53f465ae41988185826
proeszti <kispothajas@gmail.com>
2022-01-14 23:32:31 +0100

    finished

--- src/app/pricing-page/pricing-page.component.ts
+++ src/app/pricing-page/pricing-page.component.ts
@@ -14,11 +14,18 @@ interface IPriceItem {
 })
 export class PricingPageComponent implements OnInit {
 
-  priceTable: IPriceItem[] = [];
+  priceTable: IPriceItem[] = [
+    { name: "Free", price: 0, options: ["10 tanulo", "10 GB tárhely", "Email támogatás", "Dokumentáció"], btnText: "Ingyenes regisztráció" },
+    { name: "Pro", price: 15, options: ["20 tanuló", "10 GB tárhely", "Kiemelet email támogatás", "Dokumantáció"], btnText: "Kezdő lépések" },
+    { name: "Enterprice", price: 29, options: ["100 tanuló", "100 GB tárhely", "Telefonos és email támogatás", "Dokumentáció"], btnText: "Kontakt" }
+
+
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
